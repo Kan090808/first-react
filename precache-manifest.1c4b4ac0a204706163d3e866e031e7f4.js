96a1df7df0a2ae72d0e6f544df6476aa8ca43eb3
Jayden <kanjingterng@gmail.com>
2023-04-05 21:02:59 +0800

    Updates

---
+++ precache-manifest.1c4b4ac0a204706163d3e866e031e7f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b2b73853f63d872c4a",
+    "url": "/first-react/static/css/main.c888a94f.chunk.css"
+  },
+  {
+    "revision": "b8b2b73853f63d872c4a",
+    "url": "/first-react/static/js/main.b8b2b738.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/first-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/first-react/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "c86c2e26eea1091b1155",
+    "url": "/first-react/static/js/runtime~main.c86c2e26.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/first-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d58995e26e94227325f053d3b61da6cc",
+    "url": "/first-react/index.html"
+  }
+];
